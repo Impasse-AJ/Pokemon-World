f0e7a3217efaadafe88204432cf18217c634b17f
Impasse-AJ <abraham-paula@hotmail.com>
2026-05-10 22:05:33 +0200

    Dockeriza frontend backend y base de datos

--- back/src/main/java/com/pokemonworld/backend/config/SecurityConfig.java
+++ back/src/main/java/com/pokemonworld/backend/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
 
         configuracion.setAllowedOrigins(List.of(
                 "http://localhost:5173",
+                "http://127.0.0.1:5173",
                 "http://localhost:3000"
         ));
         configuracion.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
